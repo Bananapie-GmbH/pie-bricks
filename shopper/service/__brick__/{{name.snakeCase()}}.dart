0910bcfc862a220f831e31159c3e6e106690f36e
Onur Özkan <onur@bananapie.io>
2022-12-04 13:51:53 +0100

    updated service

--- shopper/service/__brick__/{{name.snakeCase()}}.dart
+++ shopper/service/__brick__/{{name.snakeCase()}}.dart
@@ -1,6 +1,13 @@
-import 'package:shopper/src/core/interfaces/shopper_api_service.dart';
+import 'package:shopper/src/core/interfaces/shopper_service.dart';
+{{#withApi}}
+import 'package:shopper/src/core/api/shopper_api.dart';
+{{/isStateless}}
 
 class {{name.pascalCase()}}Service extends IShopperService {
+
+  {{#withApi}}
+  final api = useApi();
+  {{/isStateless}}
 
   {{name.pascalCase()}}Service._();
   static final instance = {{name.pascalCase()}}Service._();
