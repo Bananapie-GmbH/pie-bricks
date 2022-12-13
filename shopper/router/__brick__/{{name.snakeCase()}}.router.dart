ed9fb1ae879dfb57d708a09f200959952761d7af
Onur Özkan <onur@bananapie.io>
2022-12-13 15:56:47 +0100

    fixes

--- shopper/router/__brick__/{{name.snakeCase()}}.router.dart
+++ shopper/router/__brick__/{{name.snakeCase()}}.router.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:shopper/shopper.dart';
 import 'package:flutter/material.dart';
-part '{{name.snakeCase()}}.gr.dart';
+part '{{name.snakeCase()}}.router.gr.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
