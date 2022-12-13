c06a9a8d11874813e2c22c41a522810748abbac8
Onur Özkan <onur@bananapie.io>
2022-12-13 10:39:10 +0100

    added router

---
+++ shopper/router/__brick__/{{name.snakeCase()}}.router.dart
@@ -0,0 +1,117 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:shopper/shopper.dart';
+import 'package:flutter/material.dart';
+part 'app_router.gr.dart';
+
+@MaterialAutoRouter(
+  replaceInRouteName: 'Page,Route',
+  routes: [
+    AutoRoute(
+      page: LoginWrapperPage,
+      children: [
+        AutoRoute(
+          page: RegisterPage,
+        ),
+        AutoRoute(
+          page: LoginPage,
+        ),
+        AutoRoute(
+          initial: true,
+          page: LoginRegisterPage,
+        ),
+      ],
+    ),
+    AutoRoute(
+        initial: true,
+        {{#useAuthGuard}}
+        guards: [AuthGuard],
+        {{/useAuthGuard}}
+        page: HomeWrapperPage,
+        children: [
+          AutoRoute(
+            initial: true,
+            page: ShopperBottomNavigationPage,
+          ),
+          AutoRoute(
+            page: CheckoutWebViewPage,
+          ),
+          AutoRoute(
+            page: DiscoveryCollectionPage,
+            children: [
+              AutoRoute(
+                page: ProductDetailPage,
+              ),
+            ],
+          ),
+          AutoRoute(
+            page: SearchResultPage,
+            children: [
+              AutoRoute(page: ProductDetailPage),
+            ],
+          ),
+          AutoRoute(page: OrderOverviewPage, children: [
+            AutoRoute(
+              page: ProductDetailPage,
+            ),
+          ]),
+          AutoRoute(page: BlogPage, children: [
+            AutoRoute(
+              page: ProductDetailPage,
+            ),
+          ]),
+          AutoRoute(page: InfluencerPage, children: [
+            AutoRoute(
+              page: ProductDetailPage,
+            ),
+            AutoRoute(
+              page: OutfitsDetailPage,
+            ),
+          ]),
+          AutoRoute(page: OutfitsDetailPage, children: [
+            AutoRoute(
+              page: ProductDetailPage,
+            ),
+            AutoRoute(
+              page: InfluencerPage,
+            ),
+          ]),
+          AutoRoute(page: OutfitsFeedPage, children: [
+            AutoRoute(
+              page: ProductDetailPage,
+            ),
+            AutoRoute(
+              page: OutfitsDetailPage,
+            ),
+            AutoRoute(
+              page: InfluencerPage,
+            ),
+          ]),
+          AutoRoute(
+            page: ProductDetailPage,
+          ),
+          AutoRoute(page: BlogsFeedPage, children: [
+            AutoRoute(
+              page: ProductDetailPage,
+            ),
+            AutoRoute(
+              page: BlogPage,
+            ),
+          ]),
+          AutoRoute(page: InfluencerFeedPage, children: [
+            AutoRoute(
+              page: InfluencerPage,
+            ),
+          ]),
+          AutoRoute(page: ProductsPage, children: [
+            AutoRoute(
+              page: ProductDetailPage,
+            ),
+          ]),
+        ])
+  ],
+)
+class AppRouter extends _$AppRouter {
+  {{#useAuthGuard}}
+  AppRouter({required super.authGuard});
+   {{/useAuthGuard}}
+}
